361c1d5c705d4119c7c72b788521182fe1cdda2a
dwarning <dietmar.warning@gmail.com>
2026-01-27 14:11:29 +0100

    simplify fortran move() function to work with gfortran

--- unix.f
+++ unix.f
@@ -24,16 +24,16 @@ C     8 byte integer used, no real*4 used
       RETURN
       END
       SUBROUTINE COPY4(FROM,TO,NWORDS)
-C     8 byte integer used, no real*4 used
+C     8 byte double used, no real*4 used
       DOUBLE PRECISION FROM(1),TO(1)
       IF(NWORDS.EQ.0) RETURN
       IF(LOCF(FROM(1)).LT.LOCF(TO(1))) GOTO 20
       DO 10 I=1,NWORDS
-       TO(I)=FROM(I)
-  10    CONTINUE
+         TO(I)=FROM(I)
+  10  CONTINUE
       RETURN
-  20    I=NWORDS
-  30    TO(I)=FROM(I)
+  20  I=NWORDS
+  30  TO(I)=FROM(I)
       I=I-1
       IF(I.NE.0) GOTO 30
       RETURN
@@ -66,19 +66,16 @@ C     8 byte integer used, no real*4 used
       IF(I.NE.0) GOTO 30
       RETURN
       END
-      SUBROUTINE MOVE(A,I,B,J,N)
-      IMPLICIT DOUBLE PRECISION (A-H,O-Z)
-      CHARACTER*150 C,D
-      DIMENSION A(*),B(*)
-      XDA=(I+N-1)/8.
-      IDA=INT(XDA)
-      IF(XDA.GT.IDA) IDA=IDA+1
-      XDB=(J+N-1)/8.
-      IDB=INT(XDB)
-      IF(XDB.GT.IDB) IDB=IDB+1
-      WRITE(C,'(18A8)') (B(M),M=1,IDB)
-      WRITE(D,'(18A8)') (A(M),M=1,IDA)
-      D(I:I+N-1)=C(J:J+N-1)
-      READ(D,'(18A8)') (A(M),M=1,IDA)
+      SUBROUTINE MOVE (A,I,B,J,N)
+      CHARACTER*1 A(1),B(1)
+C
+C THIS ROUTINE MOVES N CHARACTERS FROM CHARACTER ARRAY B  
+C TO CHARACTER ARRAY A, BEGINNING WITH THE J*TH AND I*TH  
+C CHARACTER POSITIONS, RESPECTIVELY.  
+C
+      IF (N.EQ.0) RETURN
+      DO 10 K=1,N
+         A(I+K-1)=B(J+K-1)
+   10 CONTINUE
       RETURN
       END
